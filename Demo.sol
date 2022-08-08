5bb4a0d094cf9aea22ec4f3e0dab8147a2b8a0f8
Pinotio <78278410+RonanKMcGovern@users.noreply.github.com>
2022-08-08 10:26:02 +0100

    added ethersplitter but do not know how to call the contract

--- Demo.sol
+++
@@ -1,7 +0,0 @@
-contract Demo {
-    event Echo(string message);
-
-    function echo(string calldata message) external {
-        emit Echo(message);
-    }
-}
---
+++ EtherSplitter.sol
@@ -0,0 +1,8 @@
+contract EtherSplitter {
+    function splitEther(address[] recipients) public payable {
+        uint amountForEach = msg.value / recipients.length;
+        for(uint i = 0; i < recipients.length; i++) {
+            recipients[i].transfer(amountForEach);
+        }
+    }
+}
